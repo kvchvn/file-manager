f973baccf03256c61fc0f1b0b6814500256b9c0f
Anton Kachan <aa.kvchvn@gmail.com>
2022-12-15 11:36:32 +0300

    feat: add some constants

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,13 @@
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
 export const STRANGER_NAME = 'Stranger';
-
-// export const __filename = fileURLToPath(import.meta.url);
-// export const __dirname = dirname(__filename);
 
 export const OPERATION_FAILED_ERROR = 'Operation failed.';
 export const INVALID_INPUT_ERROR = 'Invalid input.';
 
 export const NAVIGATION_COMMANDS_LIST = ['up', 'cd', 'ls'];
+export const FILES_COMMANDS_LIST = ['cat', 'add', 'rn', 'cp', 'mv', 'rm'];
+
+export const ERROR_TYPES = {
+    'ENOENT': 'ENOENT',
+    'EEXIST': 'EEXIST',
+    'INVALID_ARG': 'ERR_INVALID_ARG_TYPE',
+};
