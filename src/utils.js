b570ddd4126899e16e7172dc29fcfbc323d0ced5
Anton Kachan <aa.kvchvn@gmail.com>
2022-12-15 18:17:07 +0300

    fix: remove unused utility functions

--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,5 @@
-import { relative, resolve, parse, format } from 'path';
-
 export const print = (content = '') => {
     process.stdout.write(content + '\n');
 };
 
 export const goToNextLine = () => print();
-
-export const resolvePaths = (currentPath, newPath) => {
-    const relativePath = relative(currentPath, newPath);
-    return resolve(relativePath);
-};
-
-export const changeBasename = (path, newBasename) => {
-    const parsedPath = parse(path);
-    parsedPath.base = newBasename;
-    return format(parsedPath);
-}
